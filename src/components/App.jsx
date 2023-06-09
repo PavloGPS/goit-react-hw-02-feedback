209d910d09e098a99f0247dd0592016cf1402be9
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-06-09 21:18:28 +0300

    Update App.jsx
    first step shws statistics and let to leave feedback of three types good, neutral, bad . saves information in state with method setState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      Here will be my homework 2.1
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = feedbackOption => {
+    this.setState(prevState => ({
+      [feedbackOption]: prevState[feedbackOption] + 1,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+        <div className="feedbackOptionsSection">
+          <h2 className="sectionTitle">Please leave feedback</h2>
+          <button
+            key="good"
+            className="goodBtn"
+            onClick={() => this.onLeaveFeedback('good')}
+          >
+            Good
+          </button>
+          <button
+            key="neutral"
+            className="neutralBtn"
+            onClick={() => this.onLeaveFeedback('neutral')}
+          >
+            Neutral
+          </button>
+          <button
+            key="bad"
+            className="badBtn"
+            onClick={() => this.onLeaveFeedback('bad')}
+          >
+            Bad
+          </button>
+        </div>
+        <div className="statisticsSection">
+          <h2 className="sectionTitle">Statistics</h2>
+          <p className="statistics__item">Good: {good}</p>
+          <p className="statistics__item">Neutral: {neutral}</p>
+          <p className="statistics__item">Bad: {bad}</p>
+        </div>
+      </>
+    );
+  }
+}
